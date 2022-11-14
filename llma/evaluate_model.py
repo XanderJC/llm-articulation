811e77676574e80f2d591e7c959709dc03014b55
Alex Chan <alexjameschan@gmail.com>
2022-11-14 23:20:04 +0000

    prelim task results

---
+++ llma/evaluate_model.py
@@ -0,0 +1,99 @@
+from llma import prompt_builder
+from llma.data import load_data
+from tqdm import tqdm
+import argparse
+import openai
+import numpy as np
+import csv
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--models",
+    nargs="+",
+    default=["text-ada-001", "text-babbage-001", "text-curie-001", "text-davinci-002"],
+)
+parser.add_argument(
+    "--tasks", nargs="+", default=["french", "colour", "lowercase", "question"]
+)
+parser.add_argument("--task_description", type=bool, default=True)
+parser.add_argument("--shots", nargs="+", default=[0, 1, 2, 3, 4, 5, 6])
+parser.add_argument("--verbose", type=bool, default=True)
+parser.add_argument("--output_path", type=str, default="results.csv")
+args = parser.parse_args()
+
+print(args.tasks)
+
+if not os.path.isfile(args.output_path):
+    header = ["model", "task", "shot", "task description", "accuracy"]
+    with open(args.output_path, "w") as f:  # pylint: disable=unspecified-encoding
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+for model in args.models:
+    for task in args.tasks:
+        for shot in args.shots:
+
+            data = load_data(task)
+            base_prompt = prompt_builder(
+                task=task, num=shot, include_task=args.task_description
+            )
+            print("=========================================")
+            print("Using prompt:")
+            print(base_prompt)
+            print("=========================================")
+
+            outputs = []
+            targets = data["target"].to_numpy()
+
+            for index, row in tqdm(data.iterrows()):
+
+                prompt = base_prompt + " `" + row["input"] + "'" + "\nOutput:"
+                if args.verbose:
+                    print(prompt)
+                    print("=========================================")
+                response = openai.Completion.create(
+                    engine=model,
+                    prompt=prompt,
+                    temperature=0,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    stop="\n\n",
+                    echo=False,
+                )
+
+                try:
+                    answer = response["choices"][0].text
+                    answer = ("".join(filter(str.isdigit, answer)))[0]
+                except:
+                    answer = 2
+
+                if args.verbose:
+                    print(answer)
+                    print("Raw response:")
+                    print(response["choices"][0].text)
+                    print("=========================================")
+
+                try:
+                    outputs.append(int(answer))
+                except:
+                    outputs.append(2)
+
+            predictions = np.array(outputs)
+            print(predictions)
+            correct = np.equal(predictions, targets)
+            print(correct.mean())
+
+            header = [
+                f"{model}",
+                f"{task}",
+                f"{shot}",
+                f"{args.task_description}",
+                f"{correct.mean()}",
+            ]
+            with open(
+                args.output_path, "a"
+            ) as f:  # pylint: disable=unspecified-encoding
+                writer = csv.writer(f)
+                writer.writerow(header)
--- llma/prompts.py
+++ llma/prompts.py
@@ -1,17 +1,69 @@
 french_prompts = [
-    "Input: `Il a dit bonjour.'\nOutput: 1",
-    "Input: `I like that car over there.'\nOutput: 0",
-    "Input: `Je suis a l'ecole.'\nOutput: 1 ",
-    "Input: `I don't think that's a good idea.'\nOutput: 0",
+    "Input: `Il a dit bonjour.'\nOutput:1",
+    "Input: `I like that car over there.'\nOutput:0",
+    "Input: `Je suis a l'ecole.'\nOutput:1 ",
+    "Input: `I don't think that's a good idea.'\nOutput:0",
+    "Input: `Les artistes s'inspirent de différentes sources.'\nOutput:1 ",
+    "Input: `If you imagine and dream about something long and hard enough, nothing will happen.'\nOutput:0",
 ]
 
 
 french = {
-    "task": " output 1 if the input is in French, 0 if not.",
+    "task": " output `1' if the input is in French, `0' if not.",
     "prompts": french_prompts,
 }
 
-tasks = {"french": french}
+colour_prompts = [
+    "Input: `The sky is a bright blue today.'\nOutput:1",
+    "Input: `I like that car over there.'\nOutput:0",
+    "Input: `He looks very tired at the moment'\nOutput:1 ",
+    "Input: `I don't think that's a good idea.'\nOutput:0",
+    "Input: `The moon can look yellow when it reflects sunlight'\nOutput:1 ",
+    "Input: `The weather in Nice is great today!'\nOutput:0",
+]
+
+
+colour = {
+    "task": " output `1' if the input contains a substring that is a primary colour (red, yellow, or blue), `0' if not.",
+    "prompts": colour_prompts,
+}
+
+lowercase_prompts = [
+    "Input: `can a one-question test be used to identify psychopaths.'\nOutput:1",
+    "Input: `Nothing in particular happens.'\nOutput:0",
+    "Input: `here is a type of music that all korean people enjoy listening to'\nOutput:1 ",
+    "Input: `THERE ARE MANY AMERICAN MEN HERE'\nOutput:0",
+    "Input: `the pope is not italian today and does not have to be.'\nOutput:1",
+    "Input: `The weather in Nice is great today!'\nOutput:0",
+]
+
+
+lowercase = {
+    "task": " output `1' if the input is completely lowercase, `0' if not.",
+    "prompts": lowercase_prompts,
+}
+
+question_prompts = [
+    "Input: `What's a developed country that consumes way more seafood per person than the US?'\nOutput:1",
+    "Input: `I like that car over there.'\nOutput:0",
+    "Input: `Which country of immigrants consumes tons of chicken per person but little pork?'\nOutput:1 ",
+    "Input: `Manhunt and Postal 2 were banned in New Zealand'\nOutput:0",
+    "Input: `Are there still books that are banned by the law in England?'\nOutput:1 ",
+    "Input: `I don't think that's a good idea.'\nOutput:0",
+]
+
+
+question = {
+    "task": " output `1' if the input is a question, `0' if not.",
+    "prompts": question_prompts,
+}
+
+tasks = {
+    "french": french,
+    "colour": colour,
+    "lowercase": lowercase,
+    "question": question,
+}
 
 
 def prompt_builder(task="french", num=1, include_task=False):
